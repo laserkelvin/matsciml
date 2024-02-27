d1445fe1361ec7c941d1e72f1e507d2d90249f79
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2024-02-26 17:33:50 -0800

    refactor: making sure IPEX is loaded on matsciml start up if it's available
    This is needed to ensure XPU support is patched into PyTorch at runtime

--- matsciml/__init__.py
+++ matsciml/__init__.py
@@ -4,4 +4,7 @@ from __future__ import annotations
 # features can be checked later on
 from matsciml.common.packages import package_registry  # noqa: F401
 
+if package_registry["ipex"]:
+    import intel_extension_for_pytorch as ipex  # noqa: F401
+
 __version__ = "1.2.0"
